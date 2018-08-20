0cb61af96304a09e6eb6c81f283b9f8df83b5e86
Wouter Habets <wouterhabets@gmail.com>
2018-08-20 21:53:20 +0200

    Add user page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import LoginPage from '../pages/LoginPage.vue';
+import UserPage from '../pages/UserPage.vue';
 import ErrorPage from '../pages/ErrorPage.vue';
 
 Vue.use(Router);
@@ -17,6 +18,11 @@ export default new Router({
             path: '/login',
             name: 'login',
             component: LoginPage
+        },
+        {
+            path: '/users',
+            name: 'users',
+            component: UserPage,
         },
         {
             path: '/404',
